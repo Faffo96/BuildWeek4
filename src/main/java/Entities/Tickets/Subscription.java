2a56ad25656a5c664a1f6c9016966c5dbc124c12
Marcello Verduci <156176363+gmverduci@users.noreply.github.com>
2024-05-07 05:02:58 +0200

    Classi User, ticket, Subscription, s. Service

--- src/main/java/Entities/Tickets/Subscription.java
+++ src/main/java/Entities/Tickets/Subscription.java
@@ -1,4 +1,44 @@
 package Entities.Tickets;
 
-public class Subscription {
+import Entities.Sellers.Seller;
+import Entities.User;
+import enums.SubscriptionDuration;
+
+import javax.persistence.Enumerated;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+public class Subscription extends Service{
+
+    @Enumerated
+    private SubscriptionDuration subscriptionDuration;
+
+    @OneToOne
+    private User user;
+
+    public Subscription(LocalDate expirationDate, boolean validity, Seller seller, SubscriptionDuration subscriptionDuration, User user) {
+        super(expirationDate, validity, seller);
+        this.subscriptionDuration = subscriptionDuration;
+        this.user = user;
+    }
+
+    public Subscription(User user) {
+        this.user = user;
+    }
+
+    public SubscriptionDuration getSubscriptionDuration() {
+        return subscriptionDuration;
+    }
+
+    public void setSubscriptionDuration(SubscriptionDuration subscriptionDuration) {
+        this.subscriptionDuration = subscriptionDuration;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/Entities/Tickets/Ticket.java
+++ src/main/java/Entities/Tickets/Ticket.java
@@ -1,4 +1,42 @@
 package Entities.Tickets;
 
-public class Ticket {
+import Entities.Sellers.Seller;
+import Entities.User;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket extends Service {
+
+    private LocalDate stampDate;
+
+    @ManyToOne
+    private User user;
+
+    public Ticket() {
+    }
+
+    public Ticket(LocalDate expirationDate, boolean validity, Seller seller, LocalDate stampDate, User user) {
+        super(expirationDate, validity, seller);
+        this.stampDate = stampDate;
+        this.user = user;
+    }
+
+    public LocalDate getStampDate() {
+        return stampDate;
+    }
+
+    public void setStampDate(LocalDate stampDate) {
+        this.stampDate = stampDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
